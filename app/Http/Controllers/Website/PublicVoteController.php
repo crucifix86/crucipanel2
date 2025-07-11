28b5dabd6aa8a1f2e0f278ab3c31599be40dc743
crucifix86 <crucifix86@users.noreply.github.com>
2025-07-11 07:02:27 -0500

    Fix vote logic to match dashboard implementation

--- app/Http/Controllers/Website/PublicVoteController.php
+++ app/Http/Controllers/Website/PublicVoteController.php
@@ -13,29 +13,25 @@ class PublicVoteController extends Controller
 {
     public function index(Request $request)
     {
-        // Get all active vote sites
-        $sites = VoteSite::all();
         $vote_info = [];
-        
-        // Check cooldowns for authenticated users
-        if (Auth::check()) {
-            foreach ($sites as $site) {
-                $log = VoteLog::onCooldown($request, $site->id);
-                
-                if ($log->exists()) {
-                    $log = $log->first();
-                    if (time() < ($log->created_at->getTimestamp() + (3600 * $site->hour_limit))) {
-                        $vote_info[$site->id]['end_time'] = $log->created_at->addHours($site->hour_limit)->getTimestamp() - Carbon::now()->getTimestamp();
-                        $vote_info[$site->id]['status'] = FALSE;
-                    } else {
-                        $vote_info[$site->id]['status'] = TRUE;
-                    }
+        $sites = VoteSite::all();
+
+        foreach ($sites as $site) {
+            $log = VoteLog::onCooldown($request, $site->id);
+
+            if ($log->exists()) {
+                $log = $log->first();
+                if (time() < ($log->created_at->getTimestamp() + (3600 * $site->hour_limit))) {
+                    $vote_info[$site->id]['end_time'] = $log->created_at->addHours($site->hour_limit)->getTimestamp() - Carbon::now()->getTimestamp();
+                    $vote_info[$site->id]['status'] = FALSE;
                 } else {
                     $vote_info[$site->id]['status'] = TRUE;
                 }
+            } else {
+                $vote_info[$site->id]['status'] = TRUE;
             }
         }
-        
+
         return view('website.vote', [
             'sites' => $sites,
             'vote_info' => $vote_info
