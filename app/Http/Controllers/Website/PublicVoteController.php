a7ac5cd01c0fd5b80acd534baa66a272994d59fd
crucifix86 <crucifix86@users.noreply.github.com>
2025-07-11 07:15:27 -0500

    Redirect public vote to working dashboard vote page

--- app/Http/Controllers/Website/PublicVoteController.php
+++ app/Http/Controllers/Website/PublicVoteController.php
@@ -14,46 +14,7 @@ class PublicVoteController extends Controller
 {
     public function index(Request $request)
     {
-        $vote_info = [];
-        $sites = VoteSite::all();
-
-        foreach ($sites as $site) {
-            $log = VoteLog::onCooldown($request, $site->id);
-
-            if ($log->exists()) {
-                $log = $log->first();
-                if (time() < ($log->created_at->getTimestamp() + (3600 * $site->hour_limit))) {
-                    $vote_info[$site->id]['end_time'] = $log->created_at->addHours($site->hour_limit)->getTimestamp() - Carbon::now()->getTimestamp();
-                    $vote_info[$site->id]['status'] = FALSE;
-                } else {
-                    $vote_info[$site->id]['status'] = TRUE;
-                }
-            } else {
-                $vote_info[$site->id]['status'] = TRUE;
-            }
-        }
-
-        $arena_info = [];
-        if (Auth::check()) {
-            $arena_log = ArenaLogs::onCooldown($request, Auth::user()->ID);
-            if ($arena_log->exists()) {
-                $arena_log = $arena_log->first();
-                if (time() < $arena_log->created_at->getTimestamp() + (3600 * config('arena.time'))) {
-                    $arena_info[Auth::user()->ID]['end_time'] = $arena_log->created_at->addHours(config('arena.time'))->getTimestamp() - Carbon::now()->getTimestamp();
-                    $arena_info[Auth::user()->ID]['status'] = FALSE;
-                } else {
-                    $arena_info[Auth::user()->ID]['status'] = TRUE;
-                }
-            } else {
-                $arena_info[Auth::user()->ID]['status'] = TRUE;
-            }
-        }
-
-        return view('website.vote', [
-            'sites' => $sites,
-            'vote_info' => $vote_info,
-            'arena' => new ArenaLogs(),
-            'arena_info' => $arena_info
-        ]);
+        // Redirect to the working dashboard vote page
+        return redirect()->route('app.vote.index');
     }
 }
